9ff2a25bb6cff57c58b95a267a44bd329f2e50e6
arizaakmal <arizaakmal04@gmail.com>
2025-03-27 09:40:00 +0700

    feat: add countdown timer

--- src/components/question/Question.tsx
+++ src/components/question/Question.tsx
@@ -16,7 +16,7 @@ const Question: React.FC<QuestionProps> = ({
   const question = questionsData[questionIndex];
 
   return (
-    <div className="relative mt-20 flex h-auto w-full items-center justify-center rounded bg-indigo-700/30 p-4 text-gray-200 shadow-lg">
+    <div className="relative mt-10 flex h-auto w-full items-center justify-center rounded bg-indigo-700/30 p-4 text-gray-200 shadow-lg">
       <span className="absolute -top-3 rounded-full bg-teal-500 px-3 py-1 text-sm font-semibold text-gray-200 shadow-md">
         {questionIndex + 1} / {questionsData.length}
       </span>
